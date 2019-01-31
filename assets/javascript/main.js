5eaa43f037128a991d455fdae6ca4e90f65b9182
Mario Rodriguez <mrodrigu@northseattle.edu>
2019-01-30 16:46:58 -0800

    Animate & Stop Gif
    Added logic to animate gif on click of the static image and to stop it on subsequent click.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,5 @@
 // array of topics that will be called 
-let topics = ["dogs", "cat", "eagle", "cheetah", "elephant"];
+let topics = ["dogs", "cat", "eagle", "cheetah", "elephant", "koala"];
 
 for (let i = 0; i < topics.length; i++) {
 
@@ -12,11 +12,10 @@ for (let i = 0; i < topics.length; i++) {
         $('#buttons').prepend(btn);
 }
 
-//To Do: Implement static images for initial setup -> activate gif on click logic
-//To Do: Ratings <p> tag, and logic to only show appropriate gifs
-//TO Do: Search capabilities for user with limit returns and logic to add serach topic to topics array
-
-$('button').on('click', function() {
+//To DO: Implement static images for initial setup -> activate gif on click logic
+//To DO: Ratings <p> tag, and logic to only show appropriate gifs
+//TO DO: Search capabilities for user with limit returns and logic to add serach topic to topics array
+$("button").on("click", function() {
 
         $('#gif-images').empty();
 
@@ -33,24 +32,39 @@ $('button').on('click', function() {
                 })
                 .then(function(response) {
 
-                console.log(response);
-
                 let results = response.data;
 
                 for(let i=0; i < results.length; i++) {
 
-                        let gifDiv = $("<div>");
-
                         let topicImage = $("<img>");
 
-                        topicImage.attr("src", results[i].images.fixed_height.url)
+                        topicImage.attr("class", "gif");
 
-                        gifDiv.append(topicImage);
+                        topicImage.attr("src", results[i].images.fixed_height_still.url);
 
-                        $("#gif-images").prepend(gifDiv);
+                        topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+                        topicImage.attr("data-animate", results[i].images.fixed_height.url);
+                        topicImage.attr("data-state", "still");
 
+                        $("#gif-images").prepend(topicImage);
                 }
 
                 });
+
 });
 
+//DONE: Add functionality to animate and un-animate gif
+$("body").on("click", ".gif", function() {
+
+        console.log("entering function");
+        
+        let state = $(this).attr("data-state");
+      
+        if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+                } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+                } 
+});
